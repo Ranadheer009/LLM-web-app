46b3a46f9dbc608a73a36bdae93c545d9e1887ee
Ranadheer009 <148032063+Ranadheer009@users.noreply.github.com>
2025-07-15 16:18:03 +0530

    client.py

--- frontend/client.py
+++ frontend/client.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+backend_url = os.getenv("URL")
 
 st.title("Text genetation app")
 
 user_input = st.text_input("Enter your prompt here")
 if st.button("Generate"):
     if user_input:
-        responce = requests.post("http://backend:8000/groq", json={"prompt": user_input})
+        responce = requests.post("f{backend_url}/groq", json={"prompt": user_input})
         if responce.status_code == 200:
             st.success(responce.json().get("response", "No response from server"))
         else:
